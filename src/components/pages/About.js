e07b7f03b29f9b93609e87581f61460667b30a1d
Ruslan Hassonov <ruslan.hassonov@gmail.com>
2020-11-03 17:11:09 +0100

    Initial Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the myTodoList app v1.0.0. It has been made following a crash course.</p>   
+        </React.Fragment>
+    )
+}
